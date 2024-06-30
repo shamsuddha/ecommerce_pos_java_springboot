fac80c4f8a099341f2e5fdfd617330657158ff00
shamsuddha <shamsuddha@gmail.com>
2024-06-30 21:28:37 +0600

    first commit

---
+++ ecommerce.pos.api/src/test/java/ecommerce/pos/ecommerce/pos/api/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ecommerce.pos.ecommerce.pos.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
